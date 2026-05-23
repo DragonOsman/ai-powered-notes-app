1f5d4f681ccd245b2a75e12dff3c58d51235640a
Osman Zakir <osmanzakir90@hotmail.com>
2026-05-23 18:19:50 +0500

    added correct type info

--- models/Note.ts
+++ models/Note.ts
@@ -1,32 +1,50 @@
-import mongoose, {
+import {
   Schema,
   model,
-  models
+  models,
+  type Model
 } from "mongoose";
 
-const NoteSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
-    index: true
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  content: {
-    type: String,
-    default: ""
-  },
-  summary: {
-    type: String
-  },
-  archived: {
-    type: Boolean,
-    default: false
-  }
-}, {
-  timestamps: true
-});
+export interface INote {
+  userId: string;
+  title: string;
+  content: string;
+  summary?: string;
+  archived: boolean;
 
-export const Note = models.Note || model("Note", NoteSchema);
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const NoteSchema = new Schema<INote>(
+  {
+    userId: {
+      type: String,
+      required: true,
+      index: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      default: ""
+    },
+    summary: {
+      type: String
+    },
+    archived: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export const Note: Model<INote> =
+  models.Note ??
+  model<INote>("Note", NoteSchema)
+;
