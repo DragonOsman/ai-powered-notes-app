790b6ea8eddb5b31e65a1b28ed3d568cd5cd5e25
Osman Zakir <osmanzakir90@hotmail.com>
2026-05-23 17:46:18 +0500

    wrote getNotes function

---
+++ server/actions/notes/getNotes.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { connectToDatabase } from "@/lib/db";
+import { Note } from "@/models/Note";
+
+export async function getNotes() {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  if (!session?.user) {
+    throw new Error("Unauthorized");
+  }
+
+  await connectToDatabase();
+
+  const notes = await Note.find().sort({
+    updatedAt: -1
+  });
+
+  return JSON.parse(JSON.stringify(notes));
+}
