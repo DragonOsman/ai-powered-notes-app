8357c623570e1d0869f47f82ee478e3a3f427194
Osman Zakir <osmanzakir90@hotmail.com>
2026-09-11 04:30:58 +0500

    adding server action to get note

---
+++ server/actions/notes/getNote.ts
@@ -0,0 +1,29 @@
+"user server";
+
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { connectToDatabase } from "@/lib/db";
+import { Note } from "@/models/Note";
+
+export async function getNote(noteId: string) {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  if (!session?.user) {
+    throw new Error("Unauthorized");
+  }
+
+  await connectToDatabase();
+
+  const note = await Note.findOne({
+    _id: noteId,
+    userId: session.user.id
+  });
+
+  if (!note) {
+    return null;
+  }
+
+  return JSON.parse(JSON.stringify(note));
+}
